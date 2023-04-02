9103fd29bd5d6ed4fd0ddb911d55b8f9696fa060
Abhishek <abhisheksingh.vizag@gmail.com>
2023-04-02 13:16:04 +0530

    Authentication Successfully created with no errors #2

--- hackathonProject/login/urls.py
+++ hackathonProject/login/urls.py
@@ -5,7 +5,7 @@ from . import views                                            # Importing views
 
 # creating routes
 urlpatterns = [
-    path('', views.home, name="home"),        
+    path('', views.index, name="index"),        
     path('signup', views.signup, name="signup")  ,      
     path('signin', views.signin, name="signin")  ,      
     path('signout', views.signout, name="signout")  ,           
--- hackathonProject/patient/views.py
+++ hackathonProject/patient/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+#importing required modules
+from django.shortcuts import redirect, render
+from django.http import HttpResponse
+from django.contrib.auth.models import User                         #importing User model for authentication
+from django.contrib import messages                                 #importing messages to display message when user signups
+from django.contrib.auth import authenticate, login, logout
+from hackathonProject import settings
+from django.core.mail import send_mail 
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes,force_str
+from django.core.mail import EmailMessage,send_mail
+
+
 
 # Create your views here.
+def home(request):
+    return render(request, 'patient/home.html')
