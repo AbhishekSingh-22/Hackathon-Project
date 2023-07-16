043091625db50d2bf1d89c809bcc5ce230383b6e
Abhishek <abhisheksingh.vizag@gmail.com>
2023-07-16 19:03:21 +0530

    ready for deployment #!

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web gunicorn mysite.wsgi:application --log-file -
+web: gunicorn CCMS.wsgi  --log-file -
--- hackathonProject/hackathonProject/urls.py
+++ hackathonProject/hackathonProject/urls.py
@@ -19,9 +19,16 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static                                               # Importing views.py 
 
+from django.views.static import serve
+from django.conf.urls import url
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('login.urls')),
     path('user/', include('patient.urls')),
     path('doctor/', include('doctor.urls')),
+
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
