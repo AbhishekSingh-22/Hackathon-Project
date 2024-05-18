9f010763b87faaa427ea70fd8ed69656b17de18f
Abhishek <abhisheksingh.vizag@gmail.com>
2024-05-18 14:28:44 +0530

    Fix crashes and bugs

--- hackathonProject/hackathonProject/settings.py
+++ hackathonProject/hackathonProject/settings.py
@@ -13,8 +13,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import os
 from pathlib import Path
 from .info import *
-import django_heroku
-import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -133,7 +131,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
